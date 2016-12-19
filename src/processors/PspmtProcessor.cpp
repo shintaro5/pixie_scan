f87d03124cb3a3767c4d9534522cf42efb1ac3d7
shintaro5 <seaborgium263@gmail.com>
2016-12-19 11:44:09 -0500

    added methods GetCalibPositionX and GetCalibPositionY that deduce calibrated positions of the PSPMT

--- src/processors/PspmtProcessor.cpp
+++ src/processors/PspmtProcessor.cpp
@@ -35,6 +35,8 @@ namespace dammIds{
       const int D_RAW5=4;
       const int D_SUM=5;
       const int DD_POS_CHE=6;
+      const int DD_POSCAL_CHE=7;
+      
       
       const int D_ENERGY_TRE1=10;
       const int D_ENERGY_TRE2=11;
@@ -43,7 +45,8 @@ namespace dammIds{
       const int D_ENERGY_TRE5=14;
       const int D_ENERGY_TRESUM=15;
       const int DD_POS_TRE=16;
-      
+      const int DD_POSCAL_TRE=17;
+
       const int D_QDC1=20;
       const int D_QDC2=21;
       const int D_QDC3=22;
@@ -51,6 +54,7 @@ namespace dammIds{
       const int D_QDC5=24;
       const int D_QDCSUM=25;
       const int DD_POS_QDC=26;
+      const int DD_POSCAL_QDC=27;
       
       const int DD_SINGLE_TRACE=77;
       
@@ -61,41 +65,46 @@ using namespace std;
 using namespace dammIds::pspmt;
 
 void PspmtProcessor::DeclarePlots(void) {
-    const int posBins      = 1024; 
-    const int energyBins   = 8192;
-    const int traceBins    = 256;
-    const int traceBins2   = 512;
-    const int Bins         = 2500;
-    
-    //offset 1900
-    DeclareHistogram1D(D_RAW1, energyBins, "Pspmt1 Raw");
-    DeclareHistogram1D(D_RAW2, energyBins, "Pspmt2 Raw");
-    DeclareHistogram1D(D_RAW3, energyBins, "Pspmt3 Raw");
-    DeclareHistogram1D(D_RAW4, energyBins, "Pspmt4 Raw");
-    DeclareHistogram1D(D_RAW5, energyBins, "Pspmt Dynode");
-    DeclareHistogram1D(D_SUM,  energyBins, "Pspmt Sum");
-    DeclareHistogram2D(DD_POS_CHE, Bins, Bins, "Pspmt Pos1 Raw");
-    
-    // 1910-Trace energies 
-    DeclareHistogram1D(D_ENERGY_TRE1, energyBins, "Energy1 from trace");
-    DeclareHistogram1D(D_ENERGY_TRE2, energyBins, "Energy2 from trace");
-    DeclareHistogram1D(D_ENERGY_TRE3, energyBins, "Energy3 from trace");
-    DeclareHistogram1D(D_ENERGY_TRE4, energyBins, "Energy4 from trace");
-    DeclareHistogram1D(D_ENERGY_TRE5, energyBins, "Energy5 from trace");
-    DeclareHistogram1D(D_ENERGY_TRESUM,  energyBins, "Pspmt Sum");
-    DeclareHistogram2D(DD_POS_TRE, Bins, Bins, "Pspmt pos Raw by Trace1");
-    
-    // 1920- QDCs
-    DeclareHistogram1D(D_QDC1, energyBins, "Energy1 from QDC");
-    DeclareHistogram1D(D_QDC2, energyBins, "Energy2 from QDC");
-    DeclareHistogram1D(D_QDC3, energyBins, "Energy3 from QDC");
-    DeclareHistogram1D(D_QDC4, energyBins, "Energy4 from QDC");
-    DeclareHistogram1D(D_QDC5, energyBins, "Energy5 from QDC");
-    DeclareHistogram2D(DD_POS_QDC, posBins, posBins, "Pspmt pos Raw by QDC");
-
-    // Trace
-    DeclareHistogram2D(DD_SINGLE_TRACE, traceBins, traceBins2,"Single trace");
-    
+  
+  const int mapBins      = 32;
+  const int traceBins    = 256;
+  const int traceBins2   = 512;
+  const int posBins      = 1024; 
+  const int Bins         = 2500;
+  const int energyBins   = 8192;
+  
+  //offset 1900
+  DeclareHistogram1D(D_RAW1, energyBins, "Pspmt1 Raw");
+  DeclareHistogram1D(D_RAW2, energyBins, "Pspmt2 Raw");
+  DeclareHistogram1D(D_RAW3, energyBins, "Pspmt3 Raw");
+  DeclareHistogram1D(D_RAW4, energyBins, "Pspmt4 Raw");
+  DeclareHistogram1D(D_RAW5, energyBins, "Pspmt Dynode");
+  DeclareHistogram1D(D_SUM,  energyBins, "Pspmt Sum");
+  DeclareHistogram2D(DD_POS_CHE, Bins, Bins, "Pspmt PosRaw");
+  DeclareHistogram2D(DD_POSCAL_CHE, mapBins, mapBins, "Pspmt PosCal");
+  
+  // 1910-Trace energies 
+  DeclareHistogram1D(D_ENERGY_TRE1, energyBins, "Energy1 from trace");
+  DeclareHistogram1D(D_ENERGY_TRE2, energyBins, "Energy2 from trace");
+  DeclareHistogram1D(D_ENERGY_TRE3, energyBins, "Energy3 from trace");
+  DeclareHistogram1D(D_ENERGY_TRE4, energyBins, "Energy4 from trace");
+  DeclareHistogram1D(D_ENERGY_TRE5, energyBins, "Energy5 from trace");
+  DeclareHistogram1D(D_ENERGY_TRESUM,  energyBins, "Pspmt Sum");
+  DeclareHistogram2D(DD_POS_TRE, Bins, Bins, "Pspmt PosRaw by Trace");
+  DeclareHistogram2D(DD_POSCAL_TRE, mapBins, mapBins, "Pspmt PosCal by Trace");
+  
+  // 1920 - QDCs
+  DeclareHistogram1D(D_QDC1, energyBins, "Energy1 from QDC");
+  DeclareHistogram1D(D_QDC2, energyBins, "Energy2 from QDC");
+  DeclareHistogram1D(D_QDC3, energyBins, "Energy3 from QDC");
+  DeclareHistogram1D(D_QDC4, energyBins, "Energy4 from QDC");
+  DeclareHistogram1D(D_QDC5, energyBins, "Energy5 from QDC");
+  DeclareHistogram2D(DD_POS_QDC, posBins, posBins, "Pspmt PosRaw by QDC");
+  DeclareHistogram2D(DD_POSCAL_QDC, mapBins, mapBins, "Pspmt PosCal by QDC");
+  
+  // Trace
+  DeclareHistogram2D(DD_SINGLE_TRACE, traceBins, traceBins2,"Single trace");
+  
 }
 
 PspmtProcessor::PspmtProcessor(void) : 
@@ -150,6 +159,16 @@ bool PspmtProcessor::PreProcess(RawEvent &event){
     return false;
   }
   
+  // Tentative position-calib params // 
+  double slope     = 0.0606;
+  double intercept = 10.13;
+  double xslope=0.0623;
+  double xoffset=-10.791;
+  double yslope=0.0601;
+  double yoffset=-9.780;
+  
+  // End of tentative position-calib params
+  
   double threshold = 5; // this parameter will be in Config file
   
     double che1=0,che2=0,che3=0,che4=0,che5=0;
@@ -160,6 +179,10 @@ bool PspmtProcessor::PreProcess(RawEvent &event){
     double xtre=0,ytre=0;
     double xqdc=0,yqdc=0;
     
+    double xche_cal=0,yche_cal=0;
+    double xtre_cal=0,ytre_cal=0;
+    double xqdc_cal=0,yqdc_cal=0;
+
     static int traceNum;
     
     for (vector<ChanEvent*>::const_iterator it = pspmtEvents.begin();
@@ -169,7 +192,7 @@ bool PspmtProcessor::PreProcess(RawEvent &event){
         string subtype    = chan->GetChanID().GetSubtype();
         int    ch         = chan->GetChanID().GetLocation();
         double calEnergy  = chan->GetCalEnergy();
-        //double pspmtTime  = chan->GetTime();
+        double pspmtTime  = chan->GetTime();
         //Trace trace       = chan->GetTrace();
         
 	Trace trc  = (*it)->GetTrace();
@@ -182,6 +205,8 @@ bool PspmtProcessor::PreProcess(RawEvent &event){
         double trace_time;
         double baseline;
 	string str_ch;
+	
+
 	
 	if(subtype=="anode1"){
 	  che1= calEnergy;
@@ -201,10 +226,11 @@ bool PspmtProcessor::PreProcess(RawEvent &event){
 	}
 	
      	if(true){ 
+	  
 	  traceNum++;   	  
 	  trace_time    = trc.GetValue("filterTime");
 	  trace_energy  = trc.GetValue("filterEnergy");
-
+	  
 	  if(subtype=="anode1"){
 	    qdc1 = qdc;
 	    tre1 = en;
@@ -230,21 +256,46 @@ bool PspmtProcessor::PreProcess(RawEvent &event){
 	    tre5 = en; 
 	    plot(D_ENERGY_TRE5,tre5);
 	    plot(D_QDC5,qdc5);
-	    
 	  }
 	  
 	   if(che1>threshold && che2>threshold && che3>threshold && che4>threshold){
 	     
+	     /* new board method
 	     xche = GetPosition(che1,che2);
 	     yche = GetPosition(che3,che4);
 	     xtre = GetPosition(tre1,tre2);
 	     ytre = GetPosition(tre3,tre4);
 	     xqdc = GetPosition(qdc1,qdc2);
 	     yqdc = GetPosition(qdc3,qdc4);
+	     */ // new board method 
+	     
+	     /* old board method*/
+	     xche = GetPositionXOldboard(che1,che2,che3,che4);
+	     yche = GetPositionYOldboard(che1,che2,che3,che4);
+	     xtre = GetPositionXOldboard(tre1,tre2,tre3,tre4);
+	     ytre = GetPositionYOldboard(tre1,tre2,tre3,tre4);
+	     xqdc = GetPositionXOldboard(qdc1,qdc2,qdc3,qdc4);
+	     yqdc = GetPositionYOldboard(qdc1,qdc2,qdc3,qdc4);
+	     
+	     // different params should use different calib params, tentatively using same params
+	     xche_cal = GetCalibPositionX(xche);
+	     yche_cal = GetCalibPositionY(yche);
+	     xtre_cal = GetCalibPositionX(xtre);
+	     ytre_cal = GetCalibPositionY(ytre);
+	     xqdc_cal = GetCalibPositionX(xqdc);
+	     yqdc_cal = GetCalibPositionY(yqdc);
+	     /* end of old board */
+	     
+	     
 	     
 	     plot(DD_POS_CHE,xche,yche);
 	     plot(DD_POS_TRE,xtre,ytre);
 	     plot(DD_POS_QDC,xqdc,yqdc);
+	     
+	     plot(DD_POSCAL_CHE,xche_cal,yche_cal);
+	     plot(DD_POSCAL_TRE,xtre_cal,ytre_cal);
+	     plot(DD_POSCAL_QDC,xqdc_cal,yqdc_cal);
+	     
 	   }
 	}
 	
@@ -307,7 +358,25 @@ double PspmtProcessor::GetPositionYOldboard(double q1,double q2,double q3,double
    ybottom  = (qbottom/qsum)*512+100;
    
    // adopted ytop as yposition. ybottom is also redundant for yposition 
-   ypos=ytop;
+   ypos = ytop;
    // ypos=ybottom;
    return ypos;
  }
+
+int PspmtProcessor::GetCalibPositionX(double xraw){
+  // tentative calib params, will be in Config file
+  double xslope=0.0623;
+  double xoffset=-10.791;
+  
+  int xcal=0;
+  xcal = trunc(xslope*xraw+xoffset);
+  return xcal;
+}
+int PspmtProcessor::GetCalibPositionY(double yraw){
+  // tentative calib params, will be in Config file
+  double yslope=0.0601;
+  double yoffset=-9.780;
+  int ycal=0;
+  ycal = trunc(yslope*yraw+yoffset);
+  return ycal;
+}
